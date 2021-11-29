23e9aa39015416daa086fa9ac29cac7e6f7c4207
Roman Kolivashko <rkolivashko@gmail.com>
2021-11-29 12:17:09 -0800

    Added context access

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 class BookList extends React.Component {
+  static contextType = ThemeContext;
   render() { 
-    return <div className="book-list">
+    const { isLightTheme, light, dark} = this.context;
+    const theme = isLightTheme ? light : dark;
+    return <div className="book-list" style={{ color: theme.syntax, background: theme.bg}}>
       <ul>
-        <li>Pride and Prejudice</li>
-        <li>Winnie the Pooh</li>
-        <li>Fear and Loathing in Las Vegas</li>
+        <li style={{ background: theme.ui }}>Pride and Prejudice</li>
+        <li style={{ background: theme.ui }}>Winnie the Pooh</li>
+        <li style={{ background: theme.ui }}>Fear and Loathing in Las Vegas</li>
       </ul>
     </div>;
   }
