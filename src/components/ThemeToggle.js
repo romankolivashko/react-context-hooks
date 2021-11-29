bc2ff79bb3d48ed1e0a294c498d517fb1af10c97
Roman Kolivashko <rkolivashko@gmail.com>
2021-11-29 15:25:47 -0800

    Added functional context access

---
+++ src/components/ThemeToggle.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ThemeToggle extends React.Component {
+  render() { 
+    return (
+      <button>Toggle the theme</button>
+    );
+  }
+}
+ 
+export default ThemeToggle;
