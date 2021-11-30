78ab0fc2a0e30ab2d2f4d7c2331a18e78690b7ff
Roman Kolivashko <rkolivashko@gmail.com>
2021-11-29 17:43:55 -0800

    Added auth context with theme context together

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import BookList from './components/BookList';
 import Navbar from './components/Navbar';
 import ThemeContextProvider from './contexts/ThemeContext';
 import ThemeToggle from './components/ThemeToggle';
-import AuthContextProvider from './components/AuthContext';
+import AuthContextProvider from './contexts/AuthContext';
 
 
 function App() {
